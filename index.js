7c3d98555984dff077e863dc4ad7485dbefeea3b
Michael Wonng <michael@wonng.com>
2018-04-19 00:13:49 +1000

    fix mongodb to v0.03

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
-var colors = require('colors');
-
 module.exports = (prev, current) => {
-    let old = current.filter( a => a.id !== undefined );
-    let incoming = current.filter( a => a.id === undefined );
+    let old = current.filter( a => a._id !== undefined );
+    let incoming = current.filter( a => a._id === undefined );
     let deleted = prev.filter( a => {
-        result = current.find( e => e.id === a.id)
+        result = current.find( e => e._id === a._id)
         if (!result) {
             return a
         }
